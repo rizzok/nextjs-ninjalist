53fdf86be3407e27a4ef54e08a7f6879341a968d
rizzok <38459379+rizzok@users.noreply.github.com>
2021-09-22 20:59:10 +0200

    video 9 done

--- pages/about.js
+++ pages/about.js
@@ -1,18 +1,28 @@
+import Head from 'next/head';
+
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est architecto
-        dolorem libero, autem alias doloribus ad eaque vel fugiat facilis!
-        Officia dicta earum autem quisquam quam quos labore molestiae sint?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est architecto
-        dolorem libero, autem alias doloribus ad eaque vel fugiat facilis!
-        Officia dicta earum autem quisquam quam quos labore molestiae sint?
-      </p>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est
+          architecto dolorem libero, autem alias doloribus ad eaque vel fugiat
+          facilis! Officia dicta earum autem quisquam quam quos labore molestiae
+          sint?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est
+          architecto dolorem libero, autem alias doloribus ad eaque vel fugiat
+          facilis! Officia dicta earum autem quisquam quam quos labore molestiae
+          sint?
+        </p>
+      </div>
+    </>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,32 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Homepage</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et recusandae
-        nostrum, laboriosam minima quis suscipit itaque sint numquam, rem
-        officia explicabo sed natus quam corporis nisi distinctio saepe ullam
-        quae.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et recusandae
-        nostrum, laboriosam minima quis suscipit itaque sint numquam, rem
-        officia explicabo sed natus quam corporis nisi distinctio saepe ullam
-        quae.
-      </p>
-      <Link href="/ninjas">
-        <a>See Ninja Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Et recusandae
+          nostrum, laboriosam minima quis suscipit itaque sint numquam, rem
+          officia explicabo sed natus quam corporis nisi distinctio saepe ullam
+          quae.
+        </p>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Et recusandae
+          nostrum, laboriosam minima quis suscipit itaque sint numquam, rem
+          officia explicabo sed natus quam corporis nisi distinctio saepe ullam
+          quae.
+        </p>
+        <Link href="/ninjas">
+          <a className={styles.btn}>See Ninja Listing</a>
+        </Link>
+      </div>
+    </>
   );
 }
