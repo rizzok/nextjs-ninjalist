189c780085997d816884a1b3cf40d60b355129f4
rizzok <38459379+rizzok@users.noreply.github.com>
2021-09-22 17:06:16 +0200

    video 5 done

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est architecto
+        dolorem libero, autem alias doloribus ad eaque vel fugiat facilis!
+        Officia dicta earum autem quisquam quam quos labore molestiae sint?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est architecto
+        dolorem libero, autem alias doloribus ad eaque vel fugiat facilis!
+        Officia dicta earum autem quisquam quam quos labore molestiae sint?
+      </p>
+    </div>
+  );
+};
+
+export default About;
